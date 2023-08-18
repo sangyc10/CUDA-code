2a5af7a26439b61159bc7cd8a51ca37a527b68cc
sangyc10 <sangyc10@163.com>
2023-08-18 16:30:27 +0800

    更新程序

--- tools/common.cuh
+++ tools/common.cuh
@@ -2,6 +2,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+cudaError_t ErrorCheck(cudaError_t error_code, const char* filename, int lineNumber)
+{
+    if (error_code != cudaSuccess)
+    {
+        printf("CUDA error:\r\ncode=%d, name=%s, description=%s\r\nfile=%s, line%d\r\n",
+                error_code, cudaGetErrorName(error_code), cudaGetErrorString(error_code), filename, lineNumber);
+        return error_code;
+    }
+    return error_code;
+}
+
+
+
+void setGPU()
+{
+    // 检测计算机GPU数量
+    int iDeviceCount = 0;
+    cudaError_t error = ErrorCheck(cudaGetDeviceCount(&iDeviceCount), __FILE__, __LINE__);
+
+    if (error != cudaSuccess || iDeviceCount == 0)
+    {
+        printf("No CUDA campatable GPU found!\n");
+        exit(-1);
+    }
+    else
+    {
+        printf("The count of GPUs is %d.\n", iDeviceCount);
+    }
+    // 设置执行
+    int iDev = 0;
+    error = ErrorCheck(cudaSetDevice(iDev), __FILE__, __LINE__);
+    if (error != cudaSuccess)
+    {
+        printf("fail to set GPU 0 for computing.\n");
+        exit(-1);
+    }
+    else
+    {
+        printf("set GPU 0 for computing.\n");
+    }
+}
+
+/************************* 未包含错误检测代码 ******************************************************************************
 void setGPU()
 {
     // 检测计算机GPU数量
@@ -30,4 +73,5 @@ void setGPU()
         printf("set GPU 0 for computing.\n");
     }
 }
+*********************************************************************************************************************/
 
